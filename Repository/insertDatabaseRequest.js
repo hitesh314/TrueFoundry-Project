47f018ce2b96f8189e4c2a830ffa021662ff6724
Hitesh <hiteshjohar314@gmail.com>
2024-02-08 00:53:10 +0530

    fix: bug fix insert query

--- Repository/insertDatabaseRequest.js
+++ Repository/insertDatabaseRequest.js
@@ -1,6 +1,12 @@
 const client = require('../connection')
 exports.insertUserRequest = (parameters) => {
-
+  parameters[2] = parameters[2].map((val) => {
+    if (isNaN(val)) {
+        return val.replaceAll("'", '"');
+    } else {
+        return val;
+    }
+  });
   var query = `INSERT INTO ${parameters[0]}
   (${parameters[1].join(', ')})
   VALUES ('${parameters[2].join("', '")}')`;
